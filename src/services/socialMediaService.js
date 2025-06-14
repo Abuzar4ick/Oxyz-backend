6f95043ebd34b6f408089802eaa2f0929adc31c6
Abuzar <abuzarrrakhmonov@gmail.com>
2025-06-14 12:06:00 +0500

    From array to object. (Getting social media links)

--- src/services/socialMediaService.js
+++ src/services/socialMediaService.js
@@ -3,5 +3,5 @@ const { SocialMedia } = require('../models')
 // Router: /api/social-media
 exports.getSocialMediaLinks = async () => {
     const links = await SocialMedia.find()
-    return { success: true, data: links }
+    return { success: true, links }
 }
