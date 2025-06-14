1ca9d2083b3ecae083d51754fbe7589e32c9940a
Abuzar <abuzarrrakhmonov@gmail.com>
2025-06-14 12:10:36 +0500

    Nmadur

--- src/services/socialMediaService.js
+++ src/services/socialMediaService.js
@@ -2,6 +2,6 @@ const { SocialMedia } = require('../models')
 
 // Router: /api/social-media
 exports.getSocialMediaLinks = async () => {
-    const links = await SocialMedia.find()
+    const links = await SocialMedia.findOne()
     return { success: true, links }
 }
